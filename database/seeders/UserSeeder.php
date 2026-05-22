91602db33ad2c6865b5652d375e28ebd17d2c420
diimprasetyos <dimasid290503@gmail.com>
2026-05-22 20:16:11 +0700

    fix: super admin tenant

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -11,7 +11,7 @@ class UserSeeder extends Seeder
 {
     public function run(): void
     {
-        $tenant = DB::table('tenants')->where('slug', 'toko-elektronik')->first();
+        $tenant = DB::table('tenants')->where('slug', 'toko-1')->first();
 
         $now = now();
 
