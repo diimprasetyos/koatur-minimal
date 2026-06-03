8dcf97c99333d6f8bdfd8f6a22cc19fd1d82d457
diimprasetyos <dimasid290503@gmail.com>
2026-06-03 13:29:23 +0700

    refactor: settings migration changes

--- database/migrations/2026_05_29_022746_create_settings_table.php
+++ database/migrations/2026_05_29_022746_create_settings_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('settings', function (Blueprint $table) {
             $table->id();
-            $table->
             $table->timestamps();
         });
     }
